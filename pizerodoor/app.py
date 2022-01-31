94d6faac33721c88e2bfcea9f60123f9e65ba25a
Deepakbaskar94 <deepakbaskar@outlook.com>
2022-01-31 15:58:05 +0530

    updated

---
+++ pizerodoor/app.py
@@ -0,0 +1,74 @@
+import RPi.GPIO as GPIO
+import time
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+
+#define actuators GPIOs
+ledRed = 25
+ledYlw = 24
+
+#initialize GPIO status variables
+ledRedSts = 0
+ledYlwSts = 0
+
+# Define led pins as output
+GPIO.setup(ledRed, GPIO.OUT)   
+GPIO.setup(ledYlw, GPIO.OUT) 
+ 
+# turn leds OFF 
+GPIO.output(ledRed, GPIO.LOW)
+GPIO.output(ledYlw, GPIO.LOW)
+	
+@app.route("/")
+def index():
+    status = "Unknown"
+    return render_template('index2.html', cs = status)
+
+@app.route("/dooropen/on")
+def dooropen():
+    GPIO.output(ledRed, GPIO.HIGH)
+    time.sleep(2)
+    GPIO.output(ledRed, GPIO.LOW)
+
+    status = "Door Open"
+    return render_template('index2.html', cs = status)
+
+
+@app.route("/doorclose/on")
+def doorclose():
+    GPIO.output(ledYlw, GPIO.HIGH)
+    time.sleep(2)
+    GPIO.output(ledYlw, GPIO.LOW)
+
+    status = "Door Close"
+    return render_template('index2.html', cs = status)
+
+
+@app.route("/repeat", methods = ['POST', 'GET'])
+def repeat():
+    num = request.form['num']
+    num = int(num)
+    for x in range(num):
+        GPIO.output(ledRed, GPIO.HIGH)
+        time.sleep(1)
+        GPIO.output(ledRed, GPIO.LOW)
+        time.sleep(1)
+        GPIO.output(ledYlw, GPIO.HIGH)
+        time.sleep(1)
+        GPIO.output(ledYlw, GPIO.LOW)
+        time.sleep(1)
+    
+    status = "completed"
+    return render_template('index2.html', cs = status)
+
+if __name__ == "__main__":
+   #port = 5000 + random.randint(0, 999)
+   #print(port)
+   #url = "http://192.168.4.1:{0}".format(port)
+   #print(url)
+   app.run(use_reloader=False, host='192.168.4.1', port=80, debug=True)
+   
