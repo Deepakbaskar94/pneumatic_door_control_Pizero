6eca1029d4dfe567d14b2c788140bb762ef2a1a8
Deepakbaskar94 <deepakbaskar@outlook.com>
2022-02-04 18:00:55 +0530

    updated final

--- pizerodoor/app.py
+++ pizerodoor/app.py
@@ -8,8 +8,8 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 
 #define actuators GPIOs
-ledRed = 25
-ledYlw = 24
+ledRed = 14
+ledYlw = 15
 
 #initialize GPIO status variables
 ledRedSts = 0
@@ -48,27 +48,31 @@ def doorclose():
     return render_template('index2.html', cs = status)
 
 
-@app.route("/repeat", methods = ['POST', 'GET'])
+@app.route('/repeat', methods = ['POST', 'GET'])
 def repeat():
-    num = request.form['num']
-    num = int(num)
-    for x in range(num):
-        GPIO.output(ledRed, GPIO.HIGH)
-        time.sleep(1)
-        GPIO.output(ledRed, GPIO.LOW)
-        time.sleep(1)
-        GPIO.output(ledYlw, GPIO.HIGH)
-        time.sleep(1)
-        GPIO.output(ledYlw, GPIO.LOW)
-        time.sleep(1)
-    
-    status = "completed"
-    return render_template('index2.html', cs = status)
+    if request.method == "POST":
+        num = request.form['num1']
+        num = int(num)
+        for x in range(num):
+            GPIO.output(ledRed, GPIO.HIGH)
+            time.sleep(1)
+            GPIO.output(ledRed, GPIO.LOW)
+            time.sleep(1)
+            GPIO.output(ledYlw, GPIO.HIGH)
+            time.sleep(1)
+            GPIO.output(ledYlw, GPIO.LOW)
+            time.sleep(1)
+
+        status = "completed"
+        return render_template('index2.html', cs = status)
+    else:
+        status = "not completed"
+        render_template('index2.html', cs = status)
 
 if __name__ == "__main__":
    #port = 5000 + random.randint(0, 999)
    #print(port)
    #url = "http://192.168.4.1:{0}".format(port)
    #print(url)
-   app.run(use_reloader=False, host='192.168.4.1', port=80, debug=True)
+   app.run(use_reloader=False, host='0.0.0.0', port=3000, debug=True)
    
